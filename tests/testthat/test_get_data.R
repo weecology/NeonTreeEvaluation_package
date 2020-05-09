81cb21cc99b820dbf8a4b84e69ed7afcc2f4ac4d
bw4sz <benweinstein2010@gmail.com>
2020-05-09 14:04:58 -0700

    consolidate tests

---
+++ tests/testthat/test_get_data.R
@@ -0,0 +1,10 @@
+library(testthat)
+
+#Assert that same data files exist
+expect_true(file.exists(get_data("SJER_052","rgb")))
+expect_true(file.exists(get_data("SJER_052","lidar")))
+expect_true(file.exists(get_data("SJER_052","hyperspectral")))
+expect_true(file.exists(get_data("SJER_052","annotations")))
+
+#get data error on bad type arguments
+expect_error(get_data("SJER_052","RGB"))
