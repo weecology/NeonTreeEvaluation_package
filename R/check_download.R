89e7e7a98a68c6610b3c0b0a19e952bd01d37c40
bw4sz <benweinstein2010@gmail.com>
2020-05-17 11:15:29 -0700

    check download

---
+++ R/check_download.R
@@ -0,0 +1,12 @@
+#' Check if data has been downloaded
+#' @examples
+#' @return a set of file paths
+#' @export
+
+check_download<-function(){
+  destination<-paste(system.file(package = "NeonTreeEvaluation"),"/extdata/",sep="")
+  f<-list.files(destination)
+  if(length(f)==0){
+    stop("Data has not been downloaded to package contents. Due to data size, the package code and data live in different repositories. To download the sensor and evaluation data, use download().")
+  }
+}
